ccab6bd89d1775a2b8aff84d6f59fe07ca23c4e9
samat <stefesongomesdealmeidanetoo@gmail.com>
2023-11-28 19:44:24 -0300

    Eu sou Groot

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Stefeson
+
+Console.Clear();
+
+string pergunta;
+const string sair = "tchau";
+
+Console.Clear();
+Console.WriteLine(" Conversa com o Groot  ");
+Console.WriteLine();
+
+do
+{
+    Console.Write("Digite sua pergunta: ");
+    pergunta = Console.ReadLine()!;
+    
+    Console.WriteLine($"                          Groot: Eu sou Groot ");
+}while (pergunta != sair);
